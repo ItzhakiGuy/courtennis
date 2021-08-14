76690173a63b13d4f20a4db79c51b07d45e8c5e3
Guy Itzhaki <itzhaki1234@gmail.com>
2021-08-14 18:23:36 +0300

    Added controllers, models and ES6 Support

---
+++ client/index.js
@@ -0,0 +1,8 @@
+// import React = require("react");
+// ES6 Support
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from '.';
+
+ReactDOM.render(<App />, document.getElementById('root'));
